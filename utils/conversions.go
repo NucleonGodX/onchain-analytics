0636b85ab49109f86941303166f415c456ec2034
NucleonGodX <racerpro41@gmail.com>
2025-02-05 10:00:00 +0530

    complete conversions

--- utils/conversions.go
+++ utils/conversions.go
@@ -25,3 +25,26 @@ func WeiToGwei(wei string) float64 {
 	}
 	return value / WeiPerGwei
 }
+
+func CalculateGasFee(gasUsed, gasPrice string) float64 {
+	used, err1 := strconv.ParseFloat(gasUsed, 64)
+	price, err2 := strconv.ParseFloat(gasPrice, 64)
+	
+	if err1 != nil || err2 != nil {
+		return 0
+	}
+	
+	return (used * price) / WeiPerEth
+}
+
+func ParseInt64(s string) int64 {
+	val, err := strconv.ParseInt(s, 10, 64)
+	if err != nil {
+		return 0
+	}
+	return val
+}
+
+func NormalizeAddress(addr string) string {
+	return strings.ToLower(strings.TrimSpace(addr))
+}
